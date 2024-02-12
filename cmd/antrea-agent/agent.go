b8d1e133b46c08f3959316378961cd8275a9e1a3
Mohit Marathe <mohitmarathe23@gmail.com>
2024-02-13 01:11:04 +0530

    Complete LFX mentorship test task
    This commit is my submission for the task given in issue: #5976.
    
    Signed-off-by: Mohit Marathe <mohitmarathe23@gmail.com>

--- cmd/antrea-agent/agent.go
+++ cmd/antrea-agent/agent.go
@@ -81,6 +81,7 @@ import (
 	"antrea.io/antrea/pkg/util/k8s"
 	"antrea.io/antrea/pkg/util/lazy"
 	"antrea.io/antrea/pkg/util/podstore"
+	"antrea.io/antrea/pkg/util/env"
 	utilwait "antrea.io/antrea/pkg/util/wait"
 	"antrea.io/antrea/pkg/version"
 )
@@ -101,6 +102,8 @@ var ipv4Localhost = net.ParseIP("127.0.0.1")
 
 // run starts Antrea agent with the given options and waits for termination signal.
 func run(o *Options) error {
+	podName := env.GetPodName()
+	klog.InfoS("Github ID: mohit-marathe", "Pod name", podName)
 	klog.InfoS("Starting Antrea agent", "version", version.GetFullVersion())
 
 	// Create K8s Clientset, CRD Clientset, Multicluster CRD Clientset and SharedInformerFactory for the given config.
